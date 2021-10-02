b55c8edb9f252e29b6896c118c59cf19a0a2d8c9
Yiheng <development.henryquan@gmail.com>
2021-10-02 23:34:50 +1000

    xe.com is better and added some data for now

--- data/get_data.py
+++
@@ -1,6 +0,0 @@
-from key import API_KEY
-import requests
-
-response = requests.get(
-    'http://api.exchangeratesapi.io/v1/2021-10-01?access_key={}&symbols=AUD,CAD,PLN,MXN'.format(API_KEY))
-print(response.json())
