7850a1f14c26667891b24d6cbb5ecf684f722005
Yiheng <development.henryquan@gmail.com>
2021-10-03 15:38:11 +1100

    added AUD2SGD and convert raw json to csv
    AUD2SGD doesn't seem accurate enough

---
+++ data/convert.py
@@ -0,0 +1,18 @@
+"""
+Convert raw json data to csv for learning
+"""
+
+import json
+
+IO_NAME = 'AUD2JPY'
+if __name__ == '__main__':
+    with open('../raw/{}.json'.format(IO_NAME), 'r') as raw_data:
+        data = json.load(raw_data)
+        ten_years = data['batchList'][0]
+        rates = ten_years['rates'][1:]
+        with open('{}.csv'.format(IO_NAME), 'w') as csv:
+            csv.write('Day,Rate\n')
+            for index, rate in enumerate(rates):
+                csv.write('{},{}\n'.format(index, rate))
+
+        raw_data.close()
